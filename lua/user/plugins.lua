9badb37d30fbb9511796677a5b73dccf9f48a2ab
Clifton McIntosh <clifton.mcintosh@podium.com>
2022-05-01 17:28:07 -0500

    Add more colorschemes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,12 +60,14 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "marko-cerovac/material.nvim"
   use "dikiaap/minimalist"
   use "kristijanhusak/vim-hybrid-material"
   use "doums/darcula"
+  use "EdenEast/nightfox.nvim"
+
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
