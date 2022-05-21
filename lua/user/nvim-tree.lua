543573a40c8ad2fac29e38a994df3b36599d7225
Clifton McIntosh <clifton.mcintosh@podium.com>
2022-05-21 15:29:39 -0500

    update nvim-tree config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -21,6 +21,24 @@ vim.g.nvim_tree_icons = {
   },
 }
 
+vim.g["nvim_tree_git_hl"] = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
+vim.g["nvim_tree_highlight_opened_files"] = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
+vim.g["nvim_tree_root_folder_modifier"] = ':t' --This is the default. See :help filename-modifiers for more options
+vim.g["nvim_tree_group_empty"] = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
+vim.g["nvim_tree_symlink_arrow"] = ' >> ' -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+vim.g["nvim_tree_respect_buf_cwd"] = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
+vim.g["nvim_tree_create_in_closed_folder"] = 1 --0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
+vim.g["nvim_tree_special_files"] = { ["README.md"] = 1, Makefile = 1, MAKEFILE = 1 } -- List of filenames that gets highlighted with NvimTreeSpecialFile
+vim.g["nvim_tree_quit_on_open"] = 0
+vim.g["nvim_tree_disable_window_picker"] = 0
+vim.g["nvim_tree_show_icons"] = {
+      git = 1,
+      folders = 1,
+      files = 1,
+      folder_arrows = 1,
+      tree_width = 30,
+    }
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -64,7 +82,7 @@ nvim_tree.setup {
     ignore_list = {},
   },
   system_open = {
-    cmd = nil,
+    cmd = "",
     args = {},
   },
   filters = {
@@ -97,15 +115,18 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
+  renderer = {
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+    }
   },
 }
